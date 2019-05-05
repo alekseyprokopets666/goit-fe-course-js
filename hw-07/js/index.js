6975c535b5476d8e8ec4bbd6ab0bf42b6cf3b537
alekseyprokopets666 <44814261+alekseyprokopets666@users.noreply.github.com>
2019-05-05 14:44:01 +0300

    hw-07 added

---
+++ hw-07/js/index.js
@@ -0,0 +1,60 @@
+let body = document.body;
+
+const posts = [
+    {
+        img: "https://placeimg.com/400/150/arch",
+        title: "Post title 1",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-1.com'
+    },
+    {
+        img: "https://placeimg.com/400/150/nature",
+        title: "Post title 2",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-2.com'
+    },
+    {
+        img: "https://placeimg.com/400/150/arch",
+        title: "Post title 3",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-3.com'
+    }
+];
+
+
+function createPostCard({
+    imgSrc = '', titleText = '', textText = '', linkUrl = '',
+}) {
+    let container = document.createElement('div');
+    let postImg = document.createElement('img');
+    postImg.setAttribute('src', imgSrc);
+    let postTitle = document.createElement('h2');
+    postTitle.textContent = titleText;
+    let postText = document.createElement('p');
+    postText.textContent = textText;
+    let postLink = document.createElement('a');
+    postLink.setAttribute('href', linkUrl);
+
+    container.appendChild(postImg);
+    container.appendChild(postTitle);
+    container.appendChild(postText);
+    container.appendChild(postLink);
+    return container;
+}
+
+let postList = [];
+
+posts.forEach(post => {
+    const el = createPostCard({
+        imgSrc: post.img,
+        titleText: post.title,
+        textText: post.text,
+        linkUrl: post.link,
+    })
+    postList.push(el);
+
+});
+
+
+body.append(...postList);
+
